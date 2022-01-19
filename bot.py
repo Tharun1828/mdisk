a39426c7ac24804a508e4c01d103849f1a7b1530
DK BOTZ <97960145+DKBOTZ@users.noreply.github.com>
2022-01-19 08:08:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY', '5fd20df0c4db85798dd4f5ff3d03e3606a94f98b')
+API_KEY = environ.get('API_KEY', 'Xf95VpOFZgGy95EXXliD')
 
 bot = Client('gplink bot',
              api_id=API_ID,
@@ -33,7 +33,7 @@ async def link_handler(bot, message):
 
 
 async def get_shortlink(link):
-    url = 'https://gplinks.in/api'
+    url = 'https://mdisk.me/convertor/2x3/MZdAES'
     params = {'api': API_KEY, 'url': link}
 
     async with aiohttp.ClientSession() as session:
